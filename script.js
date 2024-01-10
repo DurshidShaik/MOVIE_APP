66e2b5d6b92493e7bb84e1ecb83d459d6cdd44f7
DurshidShaik <durshidsparshad8686@gmail.com>
2024-01-10 21:08:24 +0530

    added a google linnk

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"; //api
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"; //api path
 const IMAGEPATH = "https://image.tmdb.org/t/p/w1280"; // imagae path
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query="; //search api
-let google = "https://www.google.com/search?q=";
+let google = "https://www.google.com/search?q=";  //google path
 
 let box_container = document.querySelector(".box-container"); //main container
-let input = document.querySelector(".input");
+let input = document.querySelector(".input");  // input from user
 
-const fetchapi = async (api) => {
+const fetchapi = async (api) => {    //Starting fetch api usage
   const result = await fetch(api);
   const data = await result.json();
   show_data(data.results);
@@ -18,8 +18,10 @@ const show_data = (data) => {
   box_container.innerHTML = "";
   data.forEach((item) => {
     console.log(item);
-    const box = document.createElement("div");
-    box.classList.add("box");
+    const box = document.createElement("div");  //creating a external node to add all childs into it.
+    box.classList.add("box");  //adding class to it.
+
+
     box.innerHTML = `<img src="${
       IMAGEPATH + item.poster_path
     }" alt="" class="image">
@@ -32,7 +34,7 @@ const show_data = (data) => {
   </div>`;
     box_container.appendChild(box);
     box.addEventListener('click',()=>{
-      window.open((google + item.title),"-blank");
+      window.open((google + item.title),"-blank");    //onclicking the image an external page will be open..
     })
   });
 };
@@ -40,15 +42,15 @@ const show_data = (data) => {
 
 
 
-input.addEventListener("keyup", (event) => {
+input.addEventListener("keyup", (event) => {   //key up event for searching
   //searching the movies
   console.log(event.target.value);
  
-  if (event.target.value != "") {
+  if (event.target.value != "") {   //if target value is not equal to empty it means user not entered anything into search bar.
     fetchapi(SEARCHAPI + event.target.value);
   } else {
     fetchapi(API); //calling the fetch api
   }
 });
 
-fetchapi(API);
+fetchapi(API);  //calling the fetch function
